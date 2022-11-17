4d7de503b30d4f4e5c9d5f58153f3158a66cf2f2
ProjetoSJTA3 <114890155+ProjetoSJTA3@users.noreply.github.com>
2022-11-17 20:39:57 -0300

    log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgendaComponent } from './views/agenda/agenda.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDatepickerModule,
+    TextFieldModule,
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
